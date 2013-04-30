e3271d646c0ad7e31398f0515e68a369677850c4
Yuya Tanaka <yuya.presto@gmail.com>
2013-04-30 12:16:29 +0900

    Ignored generated files

--- MultiTouchLayoutsTest/gen/com/ypresto/android/multitouchlayouts/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ypresto.android.multitouchlayouts.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
